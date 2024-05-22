6fb6860f7093557ca805bae3600c85bd9f2a146f
chaswrig <42905430+chaswrig@users.noreply.github.com>
2024-05-22 13:55:42 -0500

    Basic Behavior Complete -- 16 by 16 grid is created when grid size is selected. Pixels change when clicked. Shake button resets pixels to white. Future state needs to prompt for grid size, grid needs to make more pixels and pixels need to shrink to remain in window. Click needs to be changed to hover.

--- javascript.js
+++ javascript.js
@@ -1,15 +1,48 @@
-//Create initial divs in container
-const container = document.querySelector("#container");
-gridSize(16);
+//Set up grid size button behavior
+const size = document.querySelector("#size");
+size.addEventListener("click", (e) =>{
+    GridSize(16);//Placeholder value. Future version will prompt for grid size.
+});
 
+//Set up shake button behavior
+const shake = document.querySelector("#shake");
+shake.addEventListener("click", (e) => {
+    ResetPixels();
+});
 
 //Changes the size of the grid
-function gridSize(gridSize){
+function GridSize(gridSize){
+    //Delete existing grid
+    const pixels = document.querySelectorAll(".pixels");
+    pixels.forEach((pixel)=>{
+        pixel.remove();
+    })
+
+    //Create new grid
     for(let i = 1; i <= gridSize; i++){
         const div = document.createElement("div");
         div.classList.toggle("pixels");
-        div.textContent = "Test"; //Testing purposes onlycontainer.appendChild(div);
+        div.id = i;
         container.appendChild(div);
     }
+
+    //Add Event Listeners to the pixels.
+    const newPixels = document.querySelectorAll(".pixels");
+    newPixels.forEach((newPixel) => {
+        newPixel.addEventListener("click",(e) => {
+            newPixel.setAttribute("style","background-color:black")
+        });
+    });
 }
+
+//Reset all pixels to white
+function ResetPixels(){
+    const pixels = document.querySelectorAll(".pixels");
+    pixels.forEach((pixel) => {
+        pixel.setAttribute("style","background-color:white")
+    });
+}
+
+
+
 
